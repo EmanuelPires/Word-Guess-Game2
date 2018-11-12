c13b073d2818606880a025009ebbdc37bfa4bb55
Emanuel Pires <emanuelpires11@gmail.com>
2018-11-12 14:57:28 -0500

    Still terrible at this

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,27 +8,84 @@
 //letters if the user letter is correct
 //have the wins +1 once all letters are guessed.
 
-var randomWordArr = ["phil collins", "styx", "sting"];
-bandGuess = randomWordArr[Math.floor(Math.random() * randomWordArr.length)];
+// document.addEventListener("DOMContentLoaded", function() {
+//   for (i = 0; i < bandGuess.length; i++) {
+//     guessingWord[i] = "_";
+//   }
+//   currentWordIndex = guessingWord.join(" ");
+//   document.getElementById("guessword").innerHTML = currentWordIndex;
+// });
 
+// //user input section
+
+// document.onkeyup = function() {
+//   var userKeyPress = String.fromCharCode(event.keyCode);
+//   lettersGuessed.push(userKeyPress);
+
+//   document.getElementById("guessedletter").innerHTML = lettersGuessed;
+//   if (eve)
+// };
+
+var selectableWords = ["phil collins", "styx", "sting"];
+
+const maxTries = 11;
+
+var guessedLetters = [];
 var currentWordIndex;
-var answerArray = [];
-var lettersGuessed = [];
-var remainingGuesses = 11;
+var guessingWord = [];
+var remainingGuesses = 0;
+var hasFinished = false;
+var gameStarted = false;
+var wins = 0;
 
-document.addEventListener("DOMContentLoaded", function() {
-  for (i = 0; i < bandGuess.length; i++) {
-    answerArray[i] = "_";
+function resetGame() {
+  remainingGuesses = maxTries;
+  gameStarted = false;
+
+  currentWordIndex = Math.floor(Math.random() * selectableWords.length);
+
+  guessedLetters = [];
+  guessingWord = [];
+
+  for (var i = 0; i < selectableWords[currentWordIndex].length; i++) {
+    guessingWord.push("_");
   }
-  currentWordIndex = answerArray.join(" ");
-  document.getElementById("guessword").innerHTML = currentWordIndex;
-});
+  updateDisplay();
+}
+function updateDisplay() {
+  document.getElementById("totalWins").innerText = wins;
+  document.getElementById("currentWord").innerText = "";
+  for (var i = 0; i < guessingWord.length; i++) {
+    document.getElementById("currentWord").innerText += guessingWord[i];
+  }
+  document.getElementById("remainingGuesses").innerText = remainingGuesses;
+  document.getElementById("guessedLetters").innerText = guessedLetters;
+  if (remainingGuesses <= 0) {
+    hasFinished = true;
+  }
+}
 
-//user input section
-
-document.onkeyup = function() {
-  var userKeyPress = String.fromCharCode(event.keyCode);
-  lettersGuessed.push(userKeyPress);
-
-  document.getElementById("guessedletter").innerHTML = lettersGuessed;
+document.onkeydown = function(event) {
+  if (hasFinished) {
+    resetGame();
+    hasFinished = false;
+  } else {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+      makeGuess(event.key.toLowerCase());
+    }
+  }
 };
+
+function makeGuess(letter) {
+  if (remainingGuesses > 0) {
+    if (!gameStarted) {
+      gameStarted = true;
+    }
+    if (guessedLetters.indexOf(letter) === -1) {
+      guessedLetters.push(letter);
+      evaluateguess(letter);
+    }
+  }
+  updateDisplay();
+  checkWin();
+}
